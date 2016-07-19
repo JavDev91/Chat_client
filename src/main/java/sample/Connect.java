46f9c680880cd129e298ceca0977e468f3f09d87
JavDev91 <Stiller3102@yandex.ru>
2016-07-19 09:46:08 +0700

    Insert SQL-query, edit connect.class

--- src/main/java/sample/Connect.java
+++ src/main/java/sample/Connect.java
@@ -5,7 +5,9 @@ package sample;
  */
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class Connect {
@@ -13,6 +15,14 @@ public class Connect {
     private String login = "root";
     private String password = "8323102";
     private String URL = "jdbc:mysql://10.41.0.111:3306";
+    private static Statement stmp;
+    private static ResultSet resultSet;
+
+    private String user;
+    private String ip;
+    private String kabinet;
+    private String komp;
+
     java.sql.Connection connection = null;
 
 
@@ -44,6 +54,23 @@ public class Connect {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public void insert_new_users(String user, String ip, String kabinet, String komp) throws SQLException {
+
+        this.user = user;
+        this.ip = ip;
+        this.kabinet = kabinet;
+        this.komp = komp;
+        String query = "INSERT INTO `taskchat`.`users` (`Login`, `IP`, `KABINET`, `KABINET.COMP`) VALUES (" + "'"+ user.toString() + "'," + "'" + ip.toString() + "'," + "'" + kabinet.toString() + "'," + "'" + komp.toString() + "')";
+
+        connection = DriverManager.getConnection(URL, login, password);
+        stmp = connection.createStatement();
+        resultSet = stmp.executeQuery(query);
+
+
+
+
     }
 
 
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -4,16 +4,44 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    AnchorPane init;
+    Stage primaryStage;
+
+    private final String STATEMENT = "";
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        this.primaryStage = primaryStage;
+        primaryStage.setTitle("Task_Chat");
+        init();
+
+
+    }
+
+    public void init() throws Exception{
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/sample.fxml"));
+        init = loader.load();
+        Scene scene = new Scene(init);
+        primaryStage.setScene(scene);
         primaryStage.show();
+
+    }
+
+    public void checkUser() {
+
+        Connect connect = new Connect();
+        connect.toConnect();
+
+
     }
 
 
